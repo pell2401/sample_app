fa26fc8d0f35bf1841b727d70db05929623a5efe
Pell2401 <makotoforlearning@gmail.com>
2023-03-27 19:16:53 +0900

    This is second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "home/top" => "home#top"
+  get "/" => "home#top"
+  get "top" => "home#top"
 end
